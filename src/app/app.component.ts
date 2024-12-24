b0268a5bce0303ab364cb084da8d5c0e268c12d1
javProg24 <javiertomalag14@gmail.com>
2024-12-24 18:10:46 -0500

    Creacion y Implementacion de FlowBite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { initFlowbite } from 'flowbite';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    initFlowbite();
+  }
   title = 'Tablas-Dinamicas';
 }
---
+++ src/app/components/tabla-data/tabla-data.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-data',
+  imports: [],
+  templateUrl: './tabla-data.component.html',
+  styleUrl: './tabla-data.component.css'
+})
+export class TablaDataComponent {
+
+}
